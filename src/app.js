ddb9b6ec4b5a3321a858f2c472d1634b835ed172
PhelipeNoleto <phelipe.noleto@outlook.com>
2021-05-26 22:26:09 -0300

    ajuste de app root

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 const jwtMiddleWare = require('../helpers/jwtMiddleware');
 const authRoute = require('./routes/authenticateRoute');
 const errorHandler = require('../helpers/errorHandler');
-const swaggerFileObject = require('../swagger_output.json');
 const logger = require('../services/logger.service');
 const usersRoute = require('./routes/usersRoute');
 const { corsOptions } = require('../config.json');
 const indexRoute = require('./routes/indexRoute');
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = '../swagger_output.json';
 const swaggerGen = require('../swagger');
 const compression = require('compression');
 const doc = require('../swagger.json');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
+const swaggerFile = path.resolve('./swagger_output.json');
+//Gera automaticamente a documentação do swagger
 swaggerGen.swaggerGen(swaggerFile, ['./src/app'], doc);
 
 const app = express();
@@ -29,7 +30,7 @@ app.use(jwtMiddleWare());
 app.use(`/v1/`, indexRoute);
 app.use(`/v1/`, authRoute);
 app.use(`/v1/users`, usersRoute);
-app.use(`/v1/doc`, swaggerUi.serve, swaggerUi.setup(swaggerFileObject));
+app.use(`/v1/doc`, swaggerUi.serve, swaggerUi.setup(require(swaggerFile)));
 //O middlewere de log deve estar entre as rotas e o manipulador de erros.
 app.use(logger);
 //O middlewere de manipulação de erros deve ser sempre o ultimo.
